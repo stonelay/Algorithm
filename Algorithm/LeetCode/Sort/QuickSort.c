0dbe30a3f332a969db76cf0950f580615a71325b
stonelay <zhanglei19910607@163.com>
2018-09-03 18:39:12 +0800

    delete

--- Algorithm/LeetCode/Sort/QuickSort.c
+++
@@ -1,9 +0,0 @@
-//
-//  QuickSort.c
-//  Algorithm
-//
-//  Created by LayZhang on 2018/8/31.
-//  Copyright © 2018年 Zhanglei. All rights reserved.
-//
-
-#include "QuickSort.h"
--- Algorithm/LeetCode/Sort/Test.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Test.cpp
-//  Algorithm
-//
-//  Created by LayZhang on 2018/9/3.
-//  Copyright © 2018年 Zhanglei. All rights reserved.
-//
-
-#include "Test.hpp"
